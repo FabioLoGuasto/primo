a2ae383518cf38d2e26b977577d277855d99f6e4
FabioLoGuasto <fabiologuasto1992@gmail.com>
2022-11-15 11:49:51 +0100

    classe payload

---
+++ src/main/java/com/application/model/Generico.java
@@ -0,0 +1,116 @@
+package com.application.model;
+public class Generico {
+	
+	
+	
+	private Long idGenerico;	
+	private String nomeGenerico;	
+	private String cognomeGenerico;	
+	private Integer etaGenerico;	
+	private Integer annoassunzioneGenerico;
+	private Integer orecontrattoGenerico;	
+	private String categoriaprotettaGenerico;	
+	private String articolo104Generico;	
+	private Integer livellocontrattoGenerico;	
+	private Integer ralGenerico;		
+	private String negozioGenerico;	
+	private Integer codicenegozioGenerico;
+	
+	public Generico() {}
+	
+	public Generico(Long idGenerico, String nomeGenerico, String cognomeGenerico, Integer etaGenerico,
+			Integer annoassunzioneGenerico, Integer orecontrattoGenerico, String categoriaprotettaGenerico,
+			String articolo104Generico, Integer livellocontrattoGenerico, Integer ralGenerico, String negozioGenerico,
+			Integer codicenegozioGenerico) {
+		super();
+		this.idGenerico = idGenerico;
+		this.nomeGenerico = nomeGenerico;
+		this.cognomeGenerico = cognomeGenerico;
+		this.etaGenerico = etaGenerico;
+		this.annoassunzioneGenerico = annoassunzioneGenerico;
+		this.orecontrattoGenerico = orecontrattoGenerico;
+		this.categoriaprotettaGenerico = categoriaprotettaGenerico;
+		this.articolo104Generico = articolo104Generico;
+		this.livellocontrattoGenerico = livellocontrattoGenerico;
+		this.ralGenerico = ralGenerico;
+		this.negozioGenerico = negozioGenerico;
+		this.codicenegozioGenerico = codicenegozioGenerico;
+	}
+	
+	public Long getIdGenerico() {
+		return idGenerico;
+	}
+	public void setIdGenerico(Long idGenerico) {
+		this.idGenerico = idGenerico;
+	}
+	public String getNomeGenerico() {
+		return nomeGenerico;
+	}
+	public void setNomeGenerico(String nomeGenerico) {
+		this.nomeGenerico = nomeGenerico;
+	}
+	public String getCognomeGenerico() {
+		return cognomeGenerico;
+	}
+	public void setCognomeGenerico(String cognomeGenerico) {
+		this.cognomeGenerico = cognomeGenerico;
+	}
+	public Integer getEtaGenerico() {
+		return etaGenerico;
+	}
+	public void setEtaGenerico(Integer etaGenerico) {
+		this.etaGenerico = etaGenerico;
+	}
+	public Integer getAnnoassunzioneGenerico() {
+		return annoassunzioneGenerico;
+	}
+	public void setAnnoassunzioneGenerico(Integer annoassunzioneGenerico) {
+		this.annoassunzioneGenerico = annoassunzioneGenerico;
+	}
+	public Integer getOrecontrattoGenerico() {
+		return orecontrattoGenerico;
+	}
+	public void setOrecontrattoGenerico(Integer orecontrattoGenerico) {
+		this.orecontrattoGenerico = orecontrattoGenerico;
+	}
+	public String getCategoriaprotettaGenerico() {
+		return categoriaprotettaGenerico;
+	}
+	public void setCategoriaprotettaGenerico(String categoriaprotettaGenerico) {
+		this.categoriaprotettaGenerico = categoriaprotettaGenerico;
+	}
+	public String getArticolo104Generico() {
+		return articolo104Generico;
+	}
+	public void setArticolo104Generico(String articolo104Generico) {
+		this.articolo104Generico = articolo104Generico;
+	}
+	public Integer getLivellocontrattoGenerico() {
+		return livellocontrattoGenerico;
+	}
+	public void setLivellocontrattoGenerico(Integer livellocontrattoGenerico) {
+		this.livellocontrattoGenerico = livellocontrattoGenerico;
+	}
+	public Integer getRalGenerico() {
+		return ralGenerico;
+	}
+	public void setRalGenerico(Integer ralGenerico) {
+		this.ralGenerico = ralGenerico;
+	}
+	public String getNegozioGenerico() {
+		return negozioGenerico;
+	}
+	public void setNegozioGenerico(String negozioGenerico) {
+		this.negozioGenerico = negozioGenerico;
+	}
+	public Integer getCodicenegozioGenerico() {
+		return codicenegozioGenerico;
+	}
+	public void setCodicenegozioGenerico(Integer codicenegozioGenerico) {
+		this.codicenegozioGenerico = codicenegozioGenerico;
+	}
+	
+	
+	
+
+}
